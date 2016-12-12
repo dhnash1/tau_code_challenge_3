6ef034fe8e380a953268049e96c9d8062959c7e9
Deforis Nash <defhnash@gmail.com>
2016-12-12 15:00:51 -0600

    finished

--- public/scripts/cc3.js
+++ public/scripts/cc3.js
@@ -1,8 +1,46 @@
-console.log( 'js' );
+console.log('js');
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
-  $( '#addJokeButton' ).on( 'click', function(){
-    console.log( 'addJokeButton on click');
-  }); // end addJokeButton on click
+$(document).ready(function() {
+    console.log('JQ');
+    getFunny();
+    $('#addJokeButton').on('click', function() {
+        console.log('addJokeButton on click');
+        var joke = $('#whoseJokeIn').val();
+        var question = $('#questionIn').val();
+        var punch = $('#punchlineIn').val();
+//I feel like a constructor would do me some good here, but its something i need to practice more.
+        var gift = {
+            person: joke,
+            q: question,
+            p: punch
+        };
+        sendThatBiz(gift);
+
+    }); // end addJokeButton on click
 }); // end doc ready
+
+function sendThatBiz(package) {
+    $.ajax({
+        type: 'POST',
+        url: '/post',
+        data: package,
+        success: function(response) {
+            $('#outputDiv').html('');
+            getFunny();
+        }
+
+    });
+}
+
+function getFunny() {
+    $.ajax({
+        type: 'GET',
+        url: '/load',
+        success: function(response) {
+            console.log(response);
+            for (var i = 0; i < response.length; i++) {
+                $('#outputDiv').append("<br><b>" + response[i].jokeQuestion + "</b><br><br>" + response[i].punchLine + "<br><p><sup>Joke by: " + response[i].whoseJoke + "</sup><p><br>"); // P.S. those HTML tags are kinda gross looking, would format differently.
+            }
+        }
+    }); // ajax call to display saved jokes to the dom
+}
--- server/app.js
+++ server/app.js
@@ -1,44 +1,64 @@
-var express = require( 'express' );
+var express = require('express');
 var app = express();
-var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
+var path = require('path');
+var bodyParser = require('body-parser');
 // create 'urlEncodedParser' in case we want to inject it for post calls:
-var urlEncodedParser = bodyParser.urlencoded( { extended: true } );
+var urlEncodedParser = bodyParser.urlencoded({
+    extended: true
+});
 // use bodyParser.urlencoded throughout the app with this:
-app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 // initial jokes provided by the client
-jokes = [
-  {
+jokes = [{
     whoseJoke: "Danny",
     jokeQuestion: "Why do scuba divers fall backwards out of boats?",
     punchLine: "If they fell forwards they’d still be in the boat"
-  },
-  {
+}, {
     whoseJoke: "Luke",
-    jokeQuestion: "Twofish are in a tank. What did one fish say to the other?",
+    jokeQuestion: "Two fish are in a tank. What did one fish say to the other?",
     punchLine: "Do you know how to drive this thing?"
-  },
-  {
+}, {
     whoseJoke: "Millie",
     jokeQuestion: "What do you call a pile of cats?",
     punchLine: "A meowntain!"
-  },
-  {
+}, {
     whoseJoke: "dEv",
     jokeQuestion: "Why should you not play cards in the forest?",
     punchLine: "Too many Cheetahs"
-  }
-];
+}];
 
 // spin up server
-app.listen( 3333, function(){
-  console.log( 'server up on 3333' );
+app.listen(3333, function() {
+    console.log('lets get funky on 3333');
 }); // end spin up server
 
-app.get( '/', function( req, res ){
-  // base url
-  console.log( 'base url hit' );
-  res.sendFile( path.resolve( 'views/index.html' ) );
+app.get('/', function(req, res) {
+    // base url
+    console.log('base url hit');
+    res.sendFile(path.resolve('views/index.html'));
 }); // end base url
 
-app.use( express.static( 'public' ) );
+app.post('/post', function(req, res) {
+
+    console.log("I got some stuff!", req.body);
+    var add = {
+        whoseJoke: req.body.person,
+        jokeQuestion: req.body.q,
+        punchLine: req.body.p
+    };
+
+    jokes.unshift(add);
+    res.send({
+        gift: jokes
+    });
+
+});
+
+app.get('/load', function(req, res) {
+
+    res.send(jokes);
+});
+
+app.use(express.static('public'));
